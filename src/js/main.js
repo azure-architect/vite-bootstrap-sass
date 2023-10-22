a7a985ca084c49184f88900f4a452573b0f817f3
azure-architect <vscoder.brad@gmail.com>
2023-10-22 16:15:38 +0800

    updated with all the changes I made from net ninjas input

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
-import '../scss/styles.scss';
-import * as Popper from "@popperjs/core";
+import '../scss/main.scss';
+import * as Popper from '@popperjs/core';
 import * as bootstrap from 'bootstrap';
